5b02fd8864120698ac36eddb22d7dcb3f2c49668
madv1ll <kevint.mtn@gmail.com>
2021-11-29 01:32:40 -0300

    nueva forma de agregar usuario

--- WebClinica/urls.py
+++ WebClinica/urls.py
@@ -1,31 +1,16 @@
-"""WebClinica URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
-from paciente.views import clinica, domicilio, nuevoMedico, nuevoPaciente, pacienteinicio, editarPaciente, eliminarPaciente, historial
+from paciente.views import clinica, domicilio, nuevoPaciente, pacienteinicio, editarPaciente, eliminarPaciente, historial, NuevoMedico
 from usuario.views import Login, logoutUser
 from django.contrib.auth.decorators import login_required
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',pacienteinicio,name = 'index'),
+    path('',login_required(pacienteinicio),name = 'index'),
     path('historial/<str:rut>/', historial, name = 'historial_clinico'),
     path('clinica/', login_required(clinica) , name= 'clinica'),
     path('domicilio/', domicilio, name= 'domicilio'),
     path('paciente/nuevo',nuevoPaciente, name='nuevoPaciente'),
-    path('medico/nuevo',nuevoMedico, name='nuevoMedico'),
+    path('medico/nuevo', NuevoMedico.as_view(), name='nuevoMedico'),
     path('editarpaciente/<str:rut>/', editarPaciente, name='editarpaciente'),
     path('eliminarpaciente/<str:rut>/', eliminarPaciente, name='eliminarpaciente'),
     path('accounts/login/', Login.as_view(), name='login'),
--- paciente/forms.py
+++ paciente/forms.py
@@ -1,13 +1,33 @@
 from django import forms
 from django.forms import fields
 from .models import Medico, Paciente
+from django.contrib.auth.forms import AuthenticationForm
 
 class PacienteForm(forms.ModelForm):
     class Meta:
         model = Paciente
-        fields = ('n_historial','lugarAtencion','rut','Nombres','Apellidos','Direccion','nombreMedico','created_date')
+        fields = ('n_historial','lugarAtencion','rut','Nombres','Apellidos','Direccion','created_date')
+
 
 class MedicoForm(forms.ModelForm):
+    password = forms.CharField(label= 'Contraseña', widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder':'Ingrese Contraseña',
+            'id': 'password'
+        }
+    ))
     class Meta:
         model = Medico
-        fields = ('rut', 'nombre', 'snombre', 'apellido', 'sapellido', 'direccion', 'especialidad')
+        fields = ('rut', 'nombre', 'snombre', 'apellido', 'sapellido', 'direccion', 'especialidad','username',)
+
+    def clean_password2(self):
+        password = self.cleaned_data.get('password')
+        return password
+ 
+    def save(self,commit = True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+            return user

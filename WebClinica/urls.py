6890fa2cc01446d5c2149b1d87844370c1e57592
madv1ll <kevint.mtn@gmail.com>
2021-12-20 22:45:43 -0300

    se arregla bug de formulario signos vitales

--- WebClinica/urls.py
+++ WebClinica/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from paciente.views import Index,  NuevoSignosVitales, SignosViews, historialDetalle, nuevoHistorialf, nuevoPaciente, editarPaciente, eliminarPaciente, historial, NuevoMedico, pacienteinicio, signosDetalle, signosVitales
+from paciente.views import Index,  NuevoSignosVitales, SignosViews, historialDetalle, nuevoHistorialf, nuevoPaciente, editarPaciente, eliminarPaciente, historial, NuevoMedico, nuevoSignosVitales, signosDetalle, signosVitales
 from usuario.views import Login, logoutUser
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
@@ -9,7 +9,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',login_required(Index.as_view()),name = 'index'),
-    #path('',login_required(pacienteinicio),name = 'index'),
     path('historial/<str:rut>/', login_required(historial), name = 'historial_clinico'),
     path('historialForm/nuevo/<str:rut>', nuevoHistorialf, name='nuevoHistorial'),
     path('historial/historialdetalle/<str:id>', historialDetalle, name='historialdetalle'),
@@ -20,6 +19,6 @@ urlpatterns = [
     path('accounts/login/', Login.as_view(), name='login'),
     path('logout/', login_required(logoutUser), name='logout'),
     path('SignosVitales/<str:rut>/', login_required(signosVitales), name='signosVitales'),
-    path('SignosVitales/<str:rut>/NuevoSignosVitales', NuevoSignosVitales.as_view(), name='signosVtalesForm'),
+    path('SignosVitales/<str:rut>/NuevoSignosVitales', nuevoSignosVitales, name='signosVtalesForm'),
     path('SignosVitales/signosdetalle/<str:id>', signosDetalle, name='signosdetalle'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

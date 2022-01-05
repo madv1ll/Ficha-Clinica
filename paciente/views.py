93c27a0bbcf7f39c9b5e97c2a6556d523d57c8e3
madv1ll <kevint.mtn@gmail.com>
2022-01-05 08:33:44 -0300

    nuevaevolucion

--- paciente/views.py
+++ paciente/views.py
@@ -186,7 +186,7 @@ def nuevaEvolucion(request, rut):
             partes = fecha.split("T")[0].split("-")
             convertida = "/".join(reversed(partes))
             post.fecha_evaluacion = convertida
-            post.hora = timezone.now()
+            post.hora = fecha
             post.save()
             return redirect ('evolucion', rut)
     else:

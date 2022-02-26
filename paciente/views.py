d3068dd1edd89ac75561cbbfe7f3ef0c0adafd78
Raul sepulveda <Raulsepulveda12@hotmail.com>
2022-02-26 14:26:39 -0300

    a

--- paciente/views.py
+++ paciente/views.py
@@ -201,7 +201,7 @@ def nuevaEvolucion(request, rut):
 
 class ReporteExcel(TemplateView):
     def get(self, request, *args, **kwargs):
-        query = Paciente.objects.filter(request.GET["rut"])
+        query = Paciente.objects.filter(rut = request.GET["rut"])
         wb = Workbook()
         bandera = True
         cont = 1

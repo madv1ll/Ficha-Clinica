5c4ff8b3420bd05610d6b826b1b3fb6bf82e8e37
madv1ll <kevint.mtn@gmail.com>
2022-03-02 13:12:32 -0300

    se agrega clase en admin.py

--- paciente/admin.py
+++ paciente/admin.py
@@ -1,6 +1,25 @@
+from django import forms
 from django.contrib import admin
 from .models import Paciente, Medico, LugarAtencion
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
 admin.site.register(Paciente)
 admin.site.register(LugarAtencion)
 admin.site.register(Medico)
+
+class UserChangeForm(forms.ModelForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    password hash display field.
+    """
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = Paciente
+        fields = ('__all__')
+
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]

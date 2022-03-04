1a26f6dfa44133bd44732789b385f5c7c6c82272
madv1ll <kevint.mtn@gmail.com>
2022-03-03 21:38:16 -0300

    reporte prueba

--- paciente/utils.py
+++ paciente/utils.py
@@ -39,29 +39,31 @@ def link_callback(uri, rel):
             )
     return path
     
-# def render_to_pdf(template_src, context_dict={}):
-#     template = get_template(template_src)
-#     html     = template.render(context_dict) 
-#     result   = BytesIO()
-#     pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")),result)
-#     if not pdf.err:
-#         return HttpResponse(result.getvalue(), content_type='application/pdf')
-#     return None
-
-def render_pdf_view(request):
-    template_path = 'user_printer.html'
-    context = {'myvar': 'this is your template context'}
-    # Create a Django response object, and specify content_type as pdf
+def render_to_pdf(template_src, context_dict={}):
+    template = get_template(template_src)
+    html     = template.render(context_dict) 
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = 'attachment; filename="report.pdf"'
-    # find the template and render it.
-    template = get_template(template_path)
-    html = template.render(context)
-
-    # create a pdf
+    result   = BytesIO()
     pisa_status = pisa.CreatePDF(
        html, dest=response, link_callback=link_callback)
     # if error then show some funy view
     if pisa_status.err:
        return HttpResponse('We had some errors <pre>' + html + '</pre>')
     return response
+
+# def render_pdf_view(request):
+#     # Create a Django response object, and specify content_type as pdf
+#     response = HttpResponse(content_type='application/pdf')
+#     response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+#     # find the template and render it.
+#     template = get_template(template_path)
+#     html = template.render(context)
+
+#     # create a pdf
+#     pisa_status = pisa.CreatePDF(
+#        html, dest=response, link_callback=link_callback)
+#     # if error then show some funy view
+#     if pisa_status.err:
+#        return HttpResponse('We had some errors <pre>' + html + '</pre>')
+#     return response

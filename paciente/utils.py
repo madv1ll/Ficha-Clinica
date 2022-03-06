daf8fbf4e19175f1d1a3ac010b6940c7f865191d
madv1ll <kevint.mtn@gmail.com>
2022-03-06 16:05:10 -0300

    prueba texto enrriquecido

--- paciente/utils.py
+++ paciente/utils.py
@@ -52,18 +52,7 @@ def render_to_pdf(template_src, context_dict={}):
        return HttpResponse('We had some errors <pre>' + html + '</pre>')
     return response
 
-# def render_pdf_view(request):
-#     # Create a Django response object, and specify content_type as pdf
-#     response = HttpResponse(content_type='application/pdf')
-#     response['Content-Disposition'] = 'attachment; filename="report.pdf"'
-#     # find the template and render it.
-#     template = get_template(template_path)
-#     html = template.render(context)
-
-#     # create a pdf
-#     pisa_status = pisa.CreatePDF(
-#        html, dest=response, link_callback=link_callback)
-#     # if error then show some funy view
-#     if pisa_status.err:
-#        return HttpResponse('We had some errors <pre>' + html + '</pre>')
-#     return response
+def get_full_path_x(request): 
+        full_path = ('http', ('', 's')[request.is_secure()], '://', 
+        request.META['HTTP_HOST'], request.path) 
+        return ''.join(full_path)

770b1710d88d5c1fa97b1912e118b702849e86de
madv1ll <kevint.mtn@gmail.com>
2022-03-24 21:44:18 -0300

    prueba nuevo paciente

--- paciente/forms.py
+++ paciente/forms.py
@@ -5,7 +5,7 @@ class PacienteForm(forms.ModelForm):
     cuidadores = Medico.objects.all()
     class Meta:
         model = Paciente
-        fields = ['lugarAtencion','rut','pnombre','snombre','papellido','sapellido','Direccion','fecha_nacimiento','nombreMedico']
+        fields = ['lugarAtencion','rut','pnombre','snombre','papellido','sapellido','Direccion','fecha_nacimiento']
 
 class MedicoForm(forms.ModelForm):
     password = forms.CharField(label= 'Contraseña', widget=forms.PasswordInput(
